0b8cabfd4d0a8b6ccc33c15fb87a883b5cac25d3
Darrell <zh0521@gmail.com>
2023-05-23 14:57:02 +0900

    Update iterators4.rs

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,9 +1,13 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
+    if num == 0 { 
+        1
+    } else {
+        (1..=num).reduce(|acc, x| acc * x).unwrap()
+    }
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
